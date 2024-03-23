213e4c8cf185ddba387632d7505604bb5fe1a171
Carlos Motta <carlos.motta.business@gmail.com>
2024-03-23 23:57:29 +0900

    Add metrics endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -18,29 +19,49 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
-func readinessHandler(w http.ResponseWriter, r *http.Request) { 
-  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Add("Content-Type", "text/plain; charset=utf-8")
   w.WriteHeader(http.StatusOK)
-  w.Write([]byte("OK"))
+  w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+  cfg.fileserverHits = 0
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte("Hits reset to 0"))
 }
 
 func main() {
-  port := ":8080" 
-  directoryPath := "."
-  mux := http.NewServeMux()
-  crsMux := middlewareCors(mux)
+	port := ":8080"
+	directoryPath := "."
+	mux := http.NewServeMux()
+	crsMux := middlewareCors(mux)
+	config := apiConfig{fileserverHits: 0}
 
-  server := &http.Server{
-    Addr: port,
-    Handler: crsMux,
-  }
+	server := &http.Server{
+		Addr:    port,
+		Handler: crsMux,
+	}
 
-  mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(directoryPath))))
-  mux.HandleFunc("/healthz", readinessHandler)
-  log.Printf("Serving on port %s", port)
+	mux.Handle("/app/*", config.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(directoryPath)))))
+	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", config.metricsHandler)
+	mux.HandleFunc("/reset", config.resetHandler)
+	log.Printf("Serving on port %s", port)
 
-  err := server.ListenAndServe()
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
